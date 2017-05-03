80e52aa32b776bc77cce83a4e16cdc92c8a0052d
Morgan Abel <morgan@mypalit.com>
2017-05-03 12:38:50 -0700

    download functions almost ready

--- www/cordova-plugin-audio-playlist.js
+++ www/cordova-plugin-audio-playlist.js
@@ -1,5 +1,12 @@
 var exec = require('cordova/exec');
 
+var downloadStatus = {
+    NONE: 0,
+    INPROGRESS: 1,
+    DONE: 2,
+    FAILED: 3
+}
+
 exports.initAudio = function(success, error) {
     audioPlugin.localForage.config({
         name: 'cordovaAudioPlaylits'
@@ -13,8 +20,8 @@ exports.clearPlaylist = function(success, error) {
 }
 
 exports.addItem = function(arg0, success, error) {
-    if (arg0 instanceof Object && arg0.hasOwnProperty("uid") && arg0.hasOwnProperty("url")) {
-        return audioPlugin.localForage.getItem("track-url-" + arg0.uid).then(function(result) {
+    if (arg0 instanceof Object && arg0.hasOwnProperty("id") && arg0.hasOwnProperty("url")) {
+        return audioPlugin.localForage.getItem("track-url-" + arg0.id).then(function(result) {
             if (null !== result) {
                 arg0.url = result;
             }
@@ -32,7 +39,7 @@ exports.addManyItems = function(arg0, success, error) {
     if (Array.isArray(arg0)) {
         return Promise.all(arg0.map(
             function(track) {
-                return audioPlugin.localForage.getItem("track-url-" + track.uid).then(function(result) {
+                return audioPlugin.localForage.getItem("track-url-" + track.id).then(function(result) {
                     if (null !== result) {
                         track.url = result;
                     }
@@ -51,8 +58,8 @@ exports.addManyItems = function(arg0, success, error) {
     }
 };
 
-exports.play = function(arg0, success, error) {
-    return execPromise(success, error, "CordovaPluginAudioPlaylist", "play", [arg0]);
+exports.play = function(success, error) {
+    return execPromise(success, error, "CordovaPluginAudioPlaylist", "play", []);
 };
 
 exports.pause = function(success, error) {
@@ -63,12 +70,99 @@ exports.watch = function(successCallback, error) {
     return exec(successCallback, error, "CordovaPluginAudioPlaylist", "watch", [])
 }
 
-exports.savePlaylistOffline = function(arg0, success, error) {
-    return execPromise(success, error, "CordovaPluginAudioPlaylist", "savePlaylistOffline", [arg0]);
+exports.savePlaylistOffline = function(inputPlaylist) {
+    var playlist =  {
+        id: "",
+        title: "",
+        albumPicture: "",
+        songs: null,
+        downloadStatus: downloadStatus.NONE
+    }
+
+    playlist = extend(playlist, inputPlaylist, true);
+    playlist.songs.forEach(function(song) {
+        song.downloadStatus = downloadStatus.NONE;
+    });
+
+    return audioPlugin.localForage.setItem("playlist-" + playlist.id, playlist).then(function() {
+        downloadPlaylist(playlist);
+    });
 }
 
-exports.getPlaylistOffline = function(arg0, success, error) {
-    return execPromise(success, error, "CordovaPluginAudioPlaylist", "getPlaylistOffline", [arg0]);
+exports.getPlaylistOffline = function(playlistId) {
+    return audioPlugin.localForage.getItem("playlist-" + playlistId);
+}
+
+exports.downloadTrack = function(track) {
+    return downloadTrack(track);
+}
+
+function downloadPlaylist(playlist) {
+    return new Promise(function(resolve, reject) {
+        audioPlugin.localForage.getItem("playlist-" + playlist.id).then(function(result) {
+            if (result !== null) {
+                playlist = result;
+            }
+
+            Promise.all(
+                playlist.songs.map(function(song) {
+                    switch (song.downloadStatus) {
+                        case downloadStatus.NONE:
+                        case downloadStatus.FAILED:
+                            return downloadTrack(song).then(function(trackResult) {
+                                audioPlugin.localForage.setItem("track-url-" + song.id).then(function(storeSongResult) {
+                                    song.url = trackResult;
+                                    song.downloadStatus = downloadStatus.DONE;
+                                });
+                            }).catch(function(trackErr) {
+                                song.downloadStatus = downloadStatus.FAILED;
+                            });
+                        default:
+                            return Promise.resolve();
+                    }
+                })
+            ).then(function(allSuccess) {
+                audioPlugin.localForage.setItem("playlist-" + playlist.id, playlist).then(function() {
+                    resolve(playlist);
+                }).catch(function(saveErr) {
+                    reject(saveErr);
+                });
+            }).catch(function(allErr) {
+                reject(allErr);
+            });
+        }); 
+    });
+}
+
+function downloadTrack(track) {
+    return new Promise(function(resolve, reject) {
+        window.requestFileSystem(cordova.file.dataDirectory, 0, function (fs) {
+            var fileTransfer = new FileTransfer();
+            var uri = encodeURI(track.url);
+
+            // Parameters passed to getFile create a new file or return the file if it already exists.
+            fs.root.getFile(track.id + '.mp3', { create: true, exclusive: false }, function (fileEntry) {
+                fileTransfer.download(
+                    uri,
+                    fileEntry,
+                    function(entry) {
+                        resolve(entry.toURL());
+                    },
+                    function(error) {
+                        reject(error);
+                    },
+                    false,
+                    {
+                        headers: {}
+                    }
+                );
+            }, function(onErrorCreateFile) {
+                reject(onErrorCreateFile);
+            });
+        }, function(onErrorFs) {
+            reject(onErrorFs);
+        });
+    });
 }
 
 function execPromise(success, error, pluginName, method, args) {
@@ -89,4 +183,31 @@ function execPromise(success, error, pluginName, method, args) {
             method,
             args);
     });
+}
+
+function extend(obj, src, overwrite) {
+    Object.keys(src).forEach(function(key) { 
+        if (obj.hasOwnProperty(key)) {
+            if (isEmpty(obj[key]) || overwrite === true) {
+                obj[key] = src[key];
+            }
+        } else {
+            obj[key] = src[key]; 
+        }
+    });
+    return obj;
+}
+
+function isEmpty(e) {
+  switch (e) {
+    case "":
+    case 0:
+    case "0":
+    case null:
+    case false:
+    case typeof this === "undefined":
+      return true;
+    default:
+      return false;
+  }
 }
